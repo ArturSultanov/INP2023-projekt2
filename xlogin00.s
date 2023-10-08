8f86e6699842565f00df3d6e9943f5cfac27207e
Artur Sultanov <sultanov.personal@gmail.com>
2023-10-08 23:17:21 +0200

    no error on m1 mac; need to test on x86

--- xlogin00.s
+++ xlogin00.s
@@ -21,11 +21,40 @@ params_sys5:    .space  8   ; misto pro ulozeni adresy pocatku
 ; CODE SEGMENT
                 .text
 main:
-        ; SEM DOPLNTE VASE RESENI
+        ; Initialize pointers and counters
+        daddi   r1, r0, login   ; r1 = pointer to the start of the string
 
+outer_loop:
+        daddi   r9, r1, 0       ; r9 = pointer for outer loop
+        daddi   r8, r0, 0       ; r8 = swap flag (0 means no swaps, 1 means at least one swap occurred)
 
-        daddi   r4, r0, login   ; vozrovy vypis: adresa login: do r4
-        jal     print_string    ; vypis pomoci print_string - viz nize
+inner_loop:
+        lb      r3, 0(r9)       ; r3 = current character
+        lb      r4, 1(r9)       ; r4 = next character
+
+        ; If end of string, end inner loop
+        beq     r4, r0, end_inner 
+
+        ; If current character > next character, swap them
+        slt     r5, r4, r3      ; set r5 = 1 if r4 < r3 (means r3 > r4)
+        beq     r5, r0, skip_swap
+
+swap:
+        sb      r3, 1(r9)       ; store value of r3 to [r9+1]
+        sb      r4, 0(r9)       ; store value of r4 to [r9]
+        daddi   r8, r0, 1       ; set swap flag to 1 (indicates a swap occurred)
+
+skip_swap:
+        daddi   r9, r9, 1       ; move r9 to the next character
+        j       inner_loop
+
+end_inner:
+        beq     r8, r0, end_outer ; if no swaps occurred, end outer loop
+        j       outer_loop
+
+end_outer:
+        daddi   r4, r0, login   ; set r4 to the start of the string
+        jal     print_string    ; call the print_string function
 
         syscall 0   ; halt
 
